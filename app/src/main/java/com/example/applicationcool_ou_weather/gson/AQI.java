28645b6f4ece7875e0cbe34ff09fd90357d31e3f
hgx <770380567@qq.com>
2020-12-25 22:43:23 +0800

    Initial commit

---
+++ app/src/main/java/com/example/applicationcool_ou_weather/gson/AQI.java
@@ -0,0 +1,15 @@
+package com.example.applicationcool_ou_weather.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+
+        public String aqi;
+
+        public String pm25;
+
+    }
+
+}
